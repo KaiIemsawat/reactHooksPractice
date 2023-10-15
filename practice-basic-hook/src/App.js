3671d09d990185cdcefb6670670fd3f9e05417d2
Kai Iemsawat <kaiiemsawat@gmail.com>
2023-10-15 08:53:26 -0400

    custom hook

--- practice-basic-hook/src/App.js
+++ practice-basic-hook/src/App.js
@@ -1,17 +1,63 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { PokemonList } from "./PokemonList";
 import axios from "axios";
+import { Pagination } from "./Pagination";
 
 function App() {
     const [pokemon, setPokemon] = useState([]);
+    const [currentPageUrl, setCurrentPageUrl] = useState(
+        "https://pokeapi.co/api/v2/pokemon"
+    );
+    const [nextPageUrl, setNextPageUrl] = useState();
+    const [previousPageUrl, setPreviousPageUrl] = useState();
+    const [isLoading, setIsLoading] = useState(true);
+
+    // Track component's mounted state
+    const isMounted = useRef(true);
 
     useEffect(() => {
-        axios.get("https://pokeapi.co/api/v2/pokemon").then((res) => {
-            setPokemon(res.data.results.map((poke) => poke.name));
-        });
-    }, []);
+        setIsLoading(true);
+        let cancel;
+        axios
+            .get(currentPageUrl, {
+                cancelToken: new axios.CancelToken((c) => (cancel = c)),
+            })
+            .then((res) => {
+                setIsLoading(false);
+                setNextPageUrl(res.data.next);
+                setPreviousPageUrl(res.data.previous);
+                setPokemon(res.data.results.map((poke) => poke.name));
+            })
+            .catch((error) => {
+                console.log(error);
+            });
 
-    return <PokemonList pokemon={pokemon} />;
+        return () => {
+            // Set the component's mounted state to false when it unmounts
+            isMounted.current = false;
+            cancel();
+        };
+    }, [currentPageUrl]);
+
+    function goToNextPage() {
+        setCurrentPageUrl(nextPageUrl);
+    }
+
+    function goToPreviousPage() {
+        setCurrentPageUrl(previousPageUrl);
+    }
+
+    if (isLoading) return <div>Loading...</div>;
+
+    return (
+        <>
+            <PokemonList pokemon={pokemon} />
+            <Pagination
+                goToNextPage={nextPageUrl ? goToNextPage : null}
+                goToPreviousPage={previousPageUrl ? goToPreviousPage : null}
+            />
+        </>
+    );
 }
 
 export default App;
--- practice-basic-hook/src/Pagination.js
+++ practice-basic-hook/src/Pagination.js
@@ -1,5 +1,12 @@
 import React from "react";
 
-export const Pagination = () => {
-    return <div>Pagination</div>;
+export const Pagination = ({ goToNextPage, goToPreviousPage }) => {
+    return (
+        <div>
+            {goToPreviousPage && (
+                <button onClick={goToPreviousPage}>Previous</button>
+            )}
+            {goToNextPage && <button onClick={goToNextPage}>Next</button>}
+        </div>
+    );
 };
