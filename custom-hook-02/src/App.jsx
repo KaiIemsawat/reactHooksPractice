822d9f1ea15ed72049a5140b2887bd1a04ed181e
Kai Iemsawat <kaiiemsawat@gmail.com>
2023-10-15 14:12:12 -0400

    usePrevious

--- custom-hook-02/src/App.jsx
+++ custom-hook-02/src/App.jsx
@@ -4,13 +4,15 @@ import TimeoutComponent from "./useTimeout/TimeoutComponent";
 import DebounceComponent from "./useDebounce/DebounceComponent";
 import UpdateEffectComponent from "./useUpdate/UpdateEffectComponent";
 import ArrayComponent from "./useArray/ArrayComponent";
+import PreviousComponent from "./usePrevious/PreviousComponent";
 
 function App() {
     // return <ToggleComponent />;
     // return <TimeoutComponent />;
     // return <DebounceComponent />;
     // return <UpdateEffectComponent />;
-    return <ArrayComponent />;
+    // return <ArrayComponent />;
+    return <PreviousComponent />;
 }
 
 export default App;
---
+++ custom-hook-02/src/usePrevious/PreviousComponent.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import usePrevious from "./usePrevious";
+
+export default function PreviousComponent() {
+    const [count, setCount] = useState(0);
+    const [name, setName] = useState("Zukkii");
+    const previousCount = usePrevious(count);
+
+    return (
+        <div>
+            <div>
+                {count} - {previousCount}
+            </div>
+            <div>{name}</div>
+            <button
+                onClick={() => setCount((currentCount) => currentCount + 1)}
+            >
+                Increment
+            </button>
+            <button onClick={() => setName("Titann")}>Change Name</button>
+        </div>
+    );
+}
