9fca77a522c401832af8ec2002223ca568ccc8b0
Kai Iemsawat <kaiiemsawat@gmail.com>
2023-10-15 13:24:51 -0400

    useArray

--- custom-hook-02/src/App.jsx
+++ custom-hook-02/src/App.jsx
@@ -3,12 +3,14 @@ import ToggleComponent from "./useToggle/ToggleComponent";
 import TimeoutComponent from "./useTimeout/TimeoutComponent";
 import DebounceComponent from "./useDebounce/DebounceComponent";
 import UpdateEffectComponent from "./useUpdate/UpdateEffectComponent";
+import ArrayComponent from "./useArray/ArrayComponent";
 
 function App() {
     // return <ToggleComponent />;
     // return <TimeoutComponent />;
     // return <DebounceComponent />;
-    return <UpdateEffectComponent />;
+    // return <UpdateEffectComponent />;
+    return <ArrayComponent />;
 }
 
 export default App;
