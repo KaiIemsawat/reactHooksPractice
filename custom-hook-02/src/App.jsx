f1abac7e4809c23020edb2b4cd88546ad644cf5d
Kai Iemsawat <kaiiemsawat@gmail.com>
2023-10-15 12:50:33 -0400

    useUpdateEffect

--- custom-hook-02/src/App.jsx
+++ custom-hook-02/src/App.jsx
@@ -2,11 +2,13 @@ import { useState } from "react";
 import ToggleComponent from "./useToggle/ToggleComponent";
 import TimeoutComponent from "./useTimeout/TimeoutComponent";
 import DebounceComponent from "./useDebounce/DebounceComponent";
+import UpdateEffectComponent from "./useUpdate/UpdateEffectComponent";
 
 function App() {
     // return <ToggleComponent />;
     // return <TimeoutComponent />;
-    return <DebounceComponent />;
+    // return <DebounceComponent />;
+    return <UpdateEffectComponent />;
 }
 
 export default App;
