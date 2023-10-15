e35419b3a19d79e1dc12712b49572e31df3b9563
Kai Iemsawat <kaiiemsawat@gmail.com>
2023-10-15 09:56:41 -0400

    custom hook useToggle

---
+++ custom-hook-02/src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import ToggleComponent from "./useToggle/ToggleComponent";
+import TimeoutComponent from "./useTimeout/TimeoutComponent";
+
+function App() {
+    // return <ToggleComponent />;
+    return <TimeoutComponent />;
+}
+
+export default App;
