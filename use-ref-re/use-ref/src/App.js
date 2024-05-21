481288b7b2aaae7cb480575a13d48e3bddf0d377
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-05-21 15:06:35 -0400

    refresh react useRef 2. use case 2

--- use-ref-re/use-ref/src/App.js
+++ use-ref-re/use-ref/src/App.js
@@ -3,21 +3,19 @@ import { useEffect, useRef, useState } from "react";
 function App() {
     const [name, setName] = useState("");
 
-    const inputRef = useRef();
+    const prevName = useRef("");
 
-    function focus() {
-        inputRef.current.focus();
-    }
+    useEffect(() => {
+        prevName.current = name;
+    }, [name]);
 
     return (
         <>
-            <input
-                ref={inputRef}
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-            />
-            <div>My name is {name}</div>
-            <button onClick={focus}>Focus</button>
+            <input value={name} onChange={(e) => setName(e.target.value)} />
+            <div>
+                My name is <h3>{name}</h3> and it used to be{" "}
+                <h3>{prevName.current}</h3>
+            </div>
         </>
     );
 }
