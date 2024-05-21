90de4a04bb1981a2a479bc269d6289b9f72cbafa
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-05-21 14:38:52 -0400

    refresh react useRef 2. use case 1

--- use-ref-re/use-ref/src/App.js
+++ use-ref-re/use-ref/src/App.js
@@ -2,18 +2,22 @@ import { useEffect, useRef, useState } from "react";
 
 function App() {
     const [name, setName] = useState("");
-    const renderCount = useRef(0);
-    // return as {current: 0}
 
-    useEffect(() => {
-        renderCount.current = renderCount.current + 1;
-    });
+    const inputRef = useRef();
+
+    function focus() {
+        inputRef.current.focus();
+    }
 
     return (
         <>
-            <input value={name} onChange={(e) => setName(e.target.value)} />
+            <input
+                ref={inputRef}
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+            />
             <div>My name is {name}</div>
-            <div>I render {renderCount.current} itmes</div>
+            <button onClick={focus}>Focus</button>
         </>
     );
 }
