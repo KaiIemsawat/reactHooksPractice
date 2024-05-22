4bdf6f81b112d1ce799d40db91722e04a829163e
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-05-21 23:52:56 -0400

    useReducer - wrap up

--- use-reducer-re/use-reducer/src/App.jsx
+++ use-reducer-re/use-reducer/src/App.jsx
@@ -1,13 +1,27 @@
 import { useReducer, useState } from "react";
+import Todo from "./Todo";
 
-const ACTIONS = {
+export const ACTIONS = {
     ADD_TODO: "add-todo",
+    TOGGLE_TODO: "toggle-todo",
+    DELETE_TODO: "delete-todo",
 };
 
 const reducer = (todos, action) => {
     switch (action.type) {
         case ACTIONS.ADD_TODO:
             return [...todos, newTodo(action.payload.name)];
+        case ACTIONS.TOGGLE_TODO:
+            return todos.map((todo) => {
+                if (todo.id === action.payload.id) {
+                    return { ...todo, complete: !todo.complete };
+                }
+                return todo;
+            });
+        case ACTIONS.DELETE_TODO:
+            return todos.filter((todo) => todo.id !== action.payload.id);
+        default:
+            return todos;
     }
 };
 
@@ -37,6 +51,9 @@ function App() {
                     onChange={(e) => setName(e.target.value)}
                 />
             </form>
+            {todos.map((todo) => (
+                <Todo key={todo.id} todo={todo} dispatch={dispatch} />
+            ))}
         </>
     );
 }
